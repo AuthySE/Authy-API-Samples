436b81d829445a6d3052227335b1f9c244b43f79
Josh <josh@twilio.com>
2016-08-12 11:36:36 -0700

    Local settings

--- sendSMS__not-jedi.sh
+++
@@ -1,9 +0,0 @@
-clear
-echo Sending SMS with Authentication Token
-echo
-echo Request
-echo curl "http://api.authy.com/protected/\$AUTHY_API_FORMAT/sms/\$AUTHY_ID?api_key=\$AUTHY_API_KEY"
-echo
-echo Response:
-curl -s "http://api.authy.com/protected/$AUTHY_API_FORMAT/sms/$AUTHY_ID?api_key=$AUTHY_API_KEY" | underscore print --outfmt pretty
-echo
