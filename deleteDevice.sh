84b6472435a68cf32902a9a7872ef3f1dd2fc1f8
Adriana Pineda <apineda@twilio.com>
2017-06-14 12:21:05 -0500

    Update comment

--- deleteDevice.sh
+++ deleteDevice.sh
@@ -1,5 +1,5 @@
 clear
-echo Delete the device
+echo Delete a sdk device
 echo
 echo Request
 echo curl -X DELETE "https://api.authy.com/protected/\$AUTHY_API_FORMAT/sdk/devices/\$AUTHY_DEVICE_ID?api_key=\$AUTHY_API_KEY"
