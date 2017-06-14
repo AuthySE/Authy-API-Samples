123b5e69448ce19642f1f7b6bc4f73dcc4562281
Adriana Pineda <apineda@twilio.com>
2017-06-14 12:18:41 -0500

    Update script to delete device

--- deleteDevice.sh
+++ deleteDevice.sh
@@ -2,8 +2,8 @@ clear
 echo Delete the device
 echo
 echo Request
-echo curl -X DELETE "https://api.authy.com/protected/$AUTHY_API_FORMAT/sdk/devices/\$AUTHY_DEVICE_ID?api_key=\$AUTHY_API_KEY"
+echo curl -X DELETE "https://api.authy.com/protected/\$AUTHY_API_FORMAT/sdk/devices/\$AUTHY_DEVICE_ID?api_key=\$AUTHY_API_KEY"
 echo
 echo Response
-curl -X DELETE "https://api.authy.com/protected/$AUTHY_API_FORMAT/sdk/devices/\$AUTHY_DEVICE_ID?api_key=\$AUTHY_API_KEY"
+curl -X DELETE "https://api.authy.com/protected/$AUTHY_API_FORMAT/sdk/devices/$AUTHY_DEVICE_ID?api_key=$AUTHY_API_KEY"
 echo
