31c83831d75b61ae6626305c39c1f362cd1347c3
Josh <josh@twilio.com>
2017-06-21 10:22:53 -0700

    removing old api key value

--- userStatus.sh
+++ userStatus.sh
@@ -2,7 +2,7 @@ clear
 echo Retrieving User Status
 echo
 echo Request
-echo curl "https://api.authy.com/protected/\$AUTHY_API_FORMAT/users/\$AUTHY_ID/status?api_key=\$AUTHY_API_KEY"
+echo curl "https://api.authy.com/protected/\$AUTHY_API_FORMAT/users/\$AUTHY_ID/status"
 echo -H "X-Authy-API-Key: \$AUTHY_API_KEY"
 echo
 echo Response
