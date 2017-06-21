038c0e3ab0254c79eaa6cd76aaa86aebc5563964
Josh <josh@twilio.com>
2017-06-21 10:19:18 -0700

    Removing underscore from user status. Cleaning up calls.

--- userStatus.sh
+++ userStatus.sh
@@ -2,10 +2,10 @@ clear
 echo Retrieving User Status
 echo
 echo Request
-echo curl -X GET "https://api.authy.com/protected/\$AUTHY_API_FORMAT/users/\$AUTHY_ID/status?api_key=\$AUTHY_API_KEY"
+echo curl "https://api.authy.com/protected/\$AUTHY_API_FORMAT/users/\$AUTHY_ID/status?api_key=\$AUTHY_API_KEY"
 echo -H "X-Authy-API-Key: \$AUTHY_API_KEY"
 echo
 echo Response
-curl --fail --silent --show-error -X GET http://api.authy.com/protected/$AUTHY_API_FORMAT/users/$AUTHY_ID/status \
--H "X-Authy-API-Key: $AUTHY_API_KEY" | underscore print --outfmt pretty
+curl http://api.authy.com/protected/$AUTHY_API_FORMAT/users/$AUTHY_ID/status \
+-H "X-Authy-API-Key: $AUTHY_API_KEY"
 echo
